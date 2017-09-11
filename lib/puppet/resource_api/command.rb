5706126394b2b7683f7fbbb5f74639a83c6045fb
David Schmitt <david.schmitt@puppet.com>
2017-09-11 17:03:06 +0100

    Fixup loading of the Command class
    Loading the file alone could fail, when the Puppet::ResourceApi module
    was not yet set up.

--- lib/puppet/resource_api/command.rb
+++ lib/puppet/resource_api/command.rb
@@ -1,30 +1,32 @@
 require 'childprocess'
 
-# A useful interface to safely run system commands
-#
-# See https://github.com/DavidS/puppet-specifications/blob/reasourceapi/language/resource-api/README.md#commands for a complete specification
-class Puppet::ResourceApi::Command
-  attr_accessor :cwd, :environment
+module Puppet::ResourceApi
+  # A useful interface to safely run system commands
+  #
+  # See https://github.com/DavidS/puppet-specifications/blob/reasourceapi/language/resource-api/README.md#commands for a complete specification
+  class Command
+    attr_accessor :cwd, :environment
 
-  attr_reader :command
+    attr_reader :command
 
-  def initialize(command)
-    @command = command
-    @cwd = '/'
-    @environment = {}
-  end
-
-  def run(*args)
-    process = ChildProcess.build(command, *args)
-    @environment.each do |k, v|
-      process.environment[k] = v
+    def initialize(command)
+      @command = command
+      @cwd = '/'
+      @environment = {}
     end
-    process.cwd = @cwd
-    process.start
-    exit_code = process.wait
 
-    raise Puppet::ResourceApi::CommandExecutionError, 'Command %{command} failed with exit code %{exit_code}' % { command: command, exit_code: exit_code } unless exit_code.zero?
-  rescue ChildProcess::LaunchError => e
-    raise Puppet::ResourceApi::CommandNotFoundError, 'Error when executing %{command}: %{error}' % { command: command, error: e.to_s }
+    def run(*args)
+      process = ChildProcess.build(command, *args)
+      @environment.each do |k, v|
+        process.environment[k] = v
+      end
+      process.cwd = @cwd
+      process.start
+      exit_code = process.wait
+
+      raise Puppet::ResourceApi::CommandExecutionError, 'Command %{command} failed with exit code %{exit_code}' % { command: command, exit_code: exit_code } unless exit_code.zero?
+    rescue ChildProcess::LaunchError => e
+      raise Puppet::ResourceApi::CommandNotFoundError, 'Error when executing %{command}: %{error}' % { command: command, error: e.to_s }
+    end
   end
 end
