16408d5fb5d689937376ba167ae3fbac4a7d1101
David Schmitt <david.schmitt@puppet.com>
2017-09-12 13:33:50 +0100

    remove version.rb from coverage testing
    version.rb is reported as 0%, since it is loaded before simplecov.
    Since its contents are trivial, it is just a distraction to see at 0%.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ if ENV['COVERAGE'] == 'yes'
   SimpleCov.start do
     track_files 'lib/**/*.rb'
 
+    add_filter 'lib/puppet/resource_api/version.rb'
+
     add_filter '/spec'
 
     # do not track vendored files
