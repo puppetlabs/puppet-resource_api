45d84d0ab0df9aae3fe6bcf2ce1d4da8e1497a01
David Schmitt <david.schmitt@puppet.com>
2018-01-18 11:06:43 +0000

    (maint) remove obsolete workaround
    PDK-666 has already been fixed, so this workaround can be removed

--- spec/fixtures/test_device/lib/puppet/provider/device_provider/device_provider.rb
+++ spec/fixtures/test_device/lib/puppet/provider/device_provider/device_provider.rb
@@ -2,13 +2,7 @@ require 'puppet/resource_api'
 
 # A example/test provider for Device support
 class Puppet::Provider::DeviceProvider::DeviceProvider
-  # TODO: remove when PDK-666 is solved
-  def canonicalize(_context, resources)
-    resources
-  end
-
   def get(_context)
-    puts Puppet::Util::NetworkDevice.current.inspect
     []
   end
 
