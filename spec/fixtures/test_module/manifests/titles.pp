8138bf82b4f4e98993908300cb9fd3c875bd03fb
David Schmitt <david.schmitt@puppet.com>
2018-03-13 11:02:45 +0000

    (PDK-822) prepare test fixtures for testing titles vs namevars

---
+++ spec/fixtures/test_module/manifests/titles.pp
@@ -0,0 +1,10 @@
+# test_module::titles
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include test_module::titles
+class test_module::titles {
+}
