606f7cfae16dae5f7c14824444ba680249c5a7ec
tphoney <tp@puppet.com>
2018-06-27 18:51:23 +0100

    (FM-7071) Adding custom facts for devices

--- lib/puppet/util/network_device/simple/device.rb
+++ lib/puppet/util/network_device/simple/device.rb
@@ -1,6 +1,7 @@
 require 'hocon'
 require 'hocon/config_syntax'
 require 'puppet/util/network_device/base'
+require 'pry'
 
 module Puppet::Util::NetworkDevice::Simple
   # A basic device class, that reads its configuration from the provided URL.
@@ -17,6 +18,15 @@ module Puppet::Util::NetworkDevice::Simple
 
     def facts
       {}
+      load_custom_facts
+    end
+
+    def load_custom_facts
+      custom_fact_folder = Puppet[:pluginfactdest]
+binding.pry
+      Puppet.debug("loading custom facts from #{custom_fact_folder}")
+      files = Dir["#{custom_fact_folder}/*.rb"]
+      Puppet.debug("files #{files}")
     end
 
     def config
