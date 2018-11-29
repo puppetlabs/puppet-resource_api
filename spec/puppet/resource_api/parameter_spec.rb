a0d1cbbc0bab797f164b23a457ba67767da3b670
David Schmitt <david@black.co.at>
2018-11-29 12:05:45 +0000

    (maint) update parameter tests

--- spec/puppet/resource_api/parameter_spec.rb
+++ spec/puppet/resource_api/parameter_spec.rb
@@ -9,70 +9,59 @@ RSpec.describe Puppet::ResourceApi::Parameter do
   let(:attribute_name) { 'some_parameter' }
   let(:data_type) { Puppet::Pops::Types::PStringType.new(nil) }
   let(:resource_hash) { { resource: {} } }
-  let(:result) { 'value' }
-  let(:value) { 'value' }
 
-  it { expect { described_class.new(nil) }.to raise_error ArgumentError, %r{wrong number of arguments} }
-  it { expect { described_class.new(type_name, data_type, attribute_name, resource_hash) }.not_to raise_error }
+  describe '#new(type_name, data_type, attribute_name, resource_hash)' do
+    it { expect { described_class.new(nil) }.to raise_error ArgumentError, %r{wrong number of arguments} }
+    it { expect { described_class.new(type_name, data_type, attribute_name, resource_hash) }.not_to raise_error }
+  end
 
   describe '#value=(value)' do
-    context 'when value is string' do
-      context 'when the value is set with string value' do
-        it('value is returned') do
-          expect(parameter.value=(value)).to eq result # rubocop:disable Style/RedundantParentheses, Layout/SpaceAroundOperators
-        end
+    it 'calls mungify and stores the munged value' do
+      expect(Puppet::ResourceApi::DataTypeHandling).to receive(:mungify)
+        .with(data_type, 'value', 'test_name.some_parameter', false)
+        .and_return('munged value')
 
-        it('value=(value) is called') do
-          allow(described_class).to receive(:value=).with(value).and_return(result)
-          described_class.value=(value) # rubocop:disable Style/RedundantParentheses, Layout/SpaceAroundOperators
-          expect(described_class).to have_received(:value=).once
-        end
-      end
+      parameter.value = 'value'
+
+      expect(parameter.value).to eq 'munged value'
+    end
+
+    it 'calls mungify and reports its error' do
+      expect(Puppet::ResourceApi::DataTypeHandling).to receive(:mungify)
+        .and_raise Exception, 'error'
+
+      expect { parameter.value = 'value' }.to raise_error Exception, 'error'
+
+      expect(parameter.value).to eq nil
     end
   end
 
   describe '#value' do
     context 'when the value is not set' do
-      it('nil is returned') do
+      it 'nil is returned' do
         expect(parameter.value).to eq nil
       end
     end
 
-    context 'when value is string' do
-      context 'when the value is set' do
-        it('value is called') do
-          allow(described_class).to receive(:value).and_return(result)
-          described_class.value
-          expect(described_class).to have_received(:value).once
-        end
-
-        it('value is returned') do
-          parameter.instance_variable_set(:@value, value)
-          expect(parameter.value).to eq result
-        end
+    context 'when the value is set' do
+      it 'value is returned' do
+        parameter.instance_variable_set(:@value, 'value')
+        expect(parameter.value).to eq 'value'
       end
     end
   end
 
   describe '#rs_value' do
     context 'when the value is not set' do
-      it('nil is returned') do
-        expect(parameter.value).to eq nil
+      it 'nil is returned' do
+        expect(parameter.rs_value).to eq nil
       end
     end
 
-    context 'when value is string' do
-      context 'when the value is set' do
-        it('value is called') do
-          allow(described_class).to receive(:rs_value).and_return(result)
-          described_class.rs_value
-          expect(described_class).to have_received(:rs_value).once
-        end
-
-        it('value is returned') do
-          parameter.instance_variable_set(:@value, value)
-          expect(parameter.rs_value).to eq result
-        end
+    context 'when the value is set' do
+      it 'value is returned' do
+        parameter.instance_variable_set(:@value, 'value')
+        expect(parameter.rs_value).to eq 'value'
       end
     end
   end
