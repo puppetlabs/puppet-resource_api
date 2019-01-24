5d89caf5c1f1e1fbca9e80366116b7bc88d04fbf
David Schmitt <david.schmitt@puppet.com>
2019-01-24 13:31:12 +0000

    (FM-7691) organise tests, and mark agent tests as such
    Loading a PuppetContext is not necessary on (the JRuby 1.7) puppet server, so stop trying to run the tests there.

--- spec/puppet/resource_api/transport_spec.rb
+++ spec/puppet/resource_api/transport_spec.rb
@@ -16,77 +16,79 @@ RSpec.describe Puppet::ResourceApi::Transport do
     described_class.instance_variable_set(:@transports, nil)
   end
 
-  context 'when registering a schema with missing keys' do
-    it { expect { described_class.register([]) }.to raise_error(Puppet::DevError, %r{requires a hash as schema}) }
-    it { expect { described_class.register({}) }.to raise_error(Puppet::DevError, %r{requires a `:name`}) }
-    it { expect { described_class.register(name: 'no connection info', desc: 'some description') }.to raise_error(Puppet::DevError, %r{requires `:connection_info`}) }
-    it { expect { described_class.register(name: 'no description') }.to raise_error(Puppet::DevError, %r{requires `:desc`}) }
-    it { expect { described_class.register(name: 'no hash attributes', desc: 'some description', connection_info: []) }.to raise_error(Puppet::DevError, %r{`:connection_info` must be a hash, not}) }
-  end
+  describe '#register(schema)' do
+    context 'when registering a schema with missing keys' do
+      it { expect { described_class.register([]) }.to raise_error(Puppet::DevError, %r{requires a hash as schema}) }
+      it { expect { described_class.register({}) }.to raise_error(Puppet::DevError, %r{requires a `:name`}) }
+      it { expect { described_class.register(name: 'no connection info', desc: 'some description') }.to raise_error(Puppet::DevError, %r{requires `:connection_info`}) }
+      it { expect { described_class.register(name: 'no description') }.to raise_error(Puppet::DevError, %r{requires `:desc`}) }
+      it { expect { described_class.register(name: 'no hash attributes', desc: 'some description', connection_info: []) }.to raise_error(Puppet::DevError, %r{`:connection_info` must be a hash, not}) }
+    end
 
-  context 'when registering a minimal transport' do
-    let(:schema) { { name: 'minimal', desc: 'a  minimal connection', connection_info: {} } }
+    context 'when registering a minimal transport' do
+      let(:schema) { { name: 'minimal', desc: 'a  minimal connection', connection_info: {} } }
 
-    it { expect { described_class.register(schema) }.not_to raise_error }
+      it { expect { described_class.register(schema) }.not_to raise_error }
 
-    context 'when re-registering a transport' do
+      context 'when re-registering a transport' do
+        it {
+          described_class.register(schema)
+          expect { described_class.register(schema) }.to raise_error(Puppet::DevError, %r{`minimal` is already registered})
+        }
+      end
+    end
+
+    context 'when registering a transport' do
+      let(:schema) do
+        {
+          name: 'a_remote_thing',
+          desc: 'basic transport',
+          connection_info: {
+            host: {
+              type: 'String',
+              desc: 'the host ip address or hostname',
+            },
+            user: {
+              type: 'String',
+              desc: 'the user to connect as',
+            },
+            password: {
+              type: 'String',
+              sensitive: true,
+              desc: 'the password to make the connection',
+            },
+          },
+        }
+      end
+
+      it 'adds to the transports register' do
+        expect { described_class.register(schema) }.not_to raise_error
+      end
+    end
+
+    context 'when registering a transport with a bad type' do
+      let(:schema) do
+        {
+          name: 'a_bad_thing',
+          desc: 'basic transport',
+          connection_info: {
+            host: {
+              type: 'garbage',
+              desc: 'the host ip address or hostname',
+            },
+          },
+        }
+      end
+
       it {
-        described_class.register(schema)
-        expect { described_class.register(schema) }.to raise_error(Puppet::DevError, %r{`minimal` is already registered})
+        expect { described_class.register(schema) }.to raise_error(
+          Puppet::DevError, %r{<garbage> is not a valid type specification}
+        )
       }
     end
   end
 
-  context 'when registering a transport' do
-    let(:schema) do
-      {
-        name: 'a_remote_thing',
-        desc: 'basic transport',
-        connection_info: {
-          host: {
-            type: 'String',
-            desc: 'the host ip address or hostname',
-          },
-          user: {
-            type: 'String',
-            desc: 'the user to connect as',
-          },
-          password: {
-            type: 'String',
-            sensitive: true,
-            desc: 'the password to make the connection',
-          },
-        },
-      }
-    end
-
-    it 'adds to the transports register' do
-      expect { described_class.register(schema) }.not_to raise_error
-    end
-  end
-
-  context 'when registering a transport with a bad type' do
-    let(:schema) do
-      {
-        name: 'a_bad_thing',
-        desc: 'basic transport',
-        connection_info: {
-          host: {
-            type: 'garbage',
-            desc: 'the host ip address or hostname',
-          },
-        },
-      }
-    end
-
-    it {
-      expect { described_class.register(schema) }.to raise_error(
-        Puppet::DevError, %r{<garbage> is not a valid type specification}
-      )
-    }
-  end
-
-  context 'when connecting to a transport' do
+  describe '#connect(name, connection_info)', agent_test: true do
     let(:name) { 'test_target' }
     let(:schema) do
       {
@@ -141,7 +143,7 @@ RSpec.describe Puppet::ResourceApi::Transport do
     end
   end
 
-  describe '#self.validate' do
+  describe '#validate(name, connection_info)', agent_test: true do
     context 'when the transport being validated has not be registered' do
       it { expect { described_class.validate('wibble', {}) }.to raise_error LoadError, %r{(no such file to load|cannot load such file) -- puppet/transport/schema/wibble} }
     end
