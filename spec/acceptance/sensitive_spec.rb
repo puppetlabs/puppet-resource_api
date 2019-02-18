1f9ede5af4ec5316b003596e4142e7d5a7b6a646
Dave Armstrong <david.armstrong@puppet.com>
2019-02-18 11:30:18 +0000

    (FM-7698) Ensure that sensitive values are handled correctly

--- spec/acceptance/sensitive_spec.rb
+++ spec/acceptance/sensitive_spec.rb
@@ -21,6 +21,22 @@ RSpec.describe 'sensitive data' do
       expect(stdout_str).not_to match %r{foo}
       expect(stdout_str).not_to match %r{warn|error}i
     end
+
+    context 'when a sensitive value is not the top level type' do
+      it 'is not exposed by a provider' do
+        stdout_str, _status = Open3.capture2e("puppet apply #{common_args} -e \"test_sensitive { bar: secret => Sensitive('foo'), "\
+          "optional_secret => Sensitive('optional foo'), variant_secret => [Sensitive('variant foo')] }\"")
+        expect(stdout_str).to match %r{redacted}
+        expect(stdout_str).not_to match %r{variant foo}
+        expect(stdout_str).not_to match %r{warn|error}i
+      end
+      it 'properly validates the sensitive type value' do
+        stdout_str, _status = Open3.capture2e("puppet apply #{common_args} -e \"test_sensitive { bar: secret => Sensitive('foo'), "\
+          "optional_secret => Sensitive('optional foo'), variant_secret => [Sensitive(134679)] }\"")
+        expect(stdout_str).to match %r{Sensitive\[String\]( value)?, got Sensitive\[Integer\]}
+        expect(stdout_str).not_to match %r{134679}
+      end
+    end
   end
 
   describe 'using `puppet resource`' do
