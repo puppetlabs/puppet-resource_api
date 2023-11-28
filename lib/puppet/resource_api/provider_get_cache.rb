90accb46683120380220f058c3b2f093782d0a7a
Sean Millichamp <sean@bruenor.org>
2023-11-28 11:33:15 -0500

    Add new ProviderGetCache class
    This helper class will provide a simple cache for Resource API
    provider state to minimize the number of get() calls to the
    provider.

---
+++ lib/puppet/resource_api/provider_get_cache.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
+module Puppet; end
+module Puppet::ResourceApi; end
+# rubocop:enable Style/Documentation
+
+# This class provides a simple caching mechanism to support minimizing get()
+# calls into a provider.
+class Puppet::ResourceApi::ProviderGetCache
+  def initialize
+    clear
+  end
+
+  def clear
+    @cache = {}
+    @cached_all = false
+  end
+
+  def all
+    raise 'all method called, but cached_all not called' unless @cached_all
+    @cache.values
+  end
+
+  def add(key, value)
+    @cache[key] = value
+  end
+
+  def get(key)
+    @cache[key]
+  end
+
+  def cached_all
+    @cached_all = true
+  end
+
+  def cached_all?
+    @cached_all
+  end
+end
