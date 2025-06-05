2a22428826e27ab4423b5d001aeb5c389ca9ea3f
Lukas Audzevicius <laudzevicius24@hotmail.com>
2025-06-05 13:20:57 +0100

    restoring several spec files to 1.9.0

--- spec/acceptance/device_spec.rb
+++ spec/acceptance/device_spec.rb
@@ -8,9 +8,16 @@ require 'tempfile'
 RSpec.describe 'exercising a device provider' do
   let(:common_args) { '--verbose --trace --strict=error --modulepath spec/fixtures' }
   let(:default_type_values) do
-    'string="meep" boolean=true integer=15 float=1.23 ensure=present variant_pattern=AE321EEF ' \
-      'url="http://www.puppet.com" boolean_param=false integer_param=99 float_param=3.21 ' \
-      'ensure_param=present variant_pattern_param=0xAE321EEF url_param="https://www.google.com"'
+    'string="meep" boolean=true integer=15 float=1.23 ensure=present variant_pattern=AE321EEF '\
+    'url="http://www.puppet.com" boolean_param=false integer_param=99 float_param=3.21 '\
+    'ensure_param=present variant_pattern_param=0xAE321EEF url_param="https://www.google.com"'
+  end
+
+  before(:all) do
+    if Gem::Version.new(Puppet::PUPPETVERSION) >= Gem::Version.new('5.3.0') && Gem::Version.new(Puppet::PUPPETVERSION) < Gem::Version.new('5.4.0')
+      # work around https://tickets.puppetlabs.com/browse/PUP-8632 and https://tickets.puppetlabs.com/browse/PUP-9047
+      FileUtils.mkdir_p(File.expand_path('~/.puppetlabs/opt/puppet/cache/devices/the_node/state'))
+    end
   end
 
   describe 'using `puppet resource`' do
@@ -25,11 +32,11 @@ RSpec.describe 'exercising a device provider' do
 
       it 'deals with canonicalized resources correctly' do
         stdout_str, status = Open3.capture2e("puppet resource #{common_args} device_provider wibble ensure=present #{default_type_values}")
-        stdmatch = 'Error: /Device_provider\[wibble\]: Could not evaluate: device_provider\[wibble\]#get has not provided canonicalized values.\n' \
-                   'Returned values:       \{:name=>"wibble", :ensure=>"present", :string=>"sample", :string_ro=>"fixed"\}\n' \
+        stdmatch = 'Error: /Device_provider\[wibble\]: Could not evaluate: device_provider\[wibble\]#get has not provided canonicalized values.\n'\
+                   'Returned values:       \{:name=>"wibble", :ensure=>"present", :string=>"sample", :string_ro=>"fixed"\}\n'\
                    'Canonicalized values:  \{:name=>"wibble", :ensure=>"present", :string=>"changed", :string_ro=>"fixed"\}'
-        expect(stdout_str).to match(/#{stdmatch}/)
-        expect(status).not_to be_success
+        expect(stdout_str).to match %r{#{stdmatch}}
+        expect(status).to be_success
       end
     end
 
@@ -38,10 +45,10 @@ RSpec.describe 'exercising a device provider' do
 
       it 'deals with canonicalized resources correctly' do
         stdout_str, status = Open3.capture2e("puppet resource #{common_args} device_provider wibble ensure=present #{default_type_values}")
-        stdmatch = 'Warning: device_provider\[wibble\]#get has not provided canonicalized values.\n' \
-                   'Returned values:       \{:name=>"wibble", :ensure=>"present", :string=>"sample", :string_ro=>"fixed"\}\n' \
+        stdmatch = 'Warning: device_provider\[wibble\]#get has not provided canonicalized values.\n'\
+                   'Returned values:       \{:name=>"wibble", :ensure=>"present", :string=>"sample", :string_ro=>"fixed"\}\n'\
                    'Canonicalized values:  \{:name=>"wibble", :ensure=>"present", :string=>"changed", :string_ro=>"fixed"\}'
-        expect(stdout_str).to match(/#{stdmatch}/)
+        expect(stdout_str).to match %r{#{stdmatch}}
         expect(status).to be_success
       end
     end
@@ -54,7 +61,7 @@ RSpec.describe 'exercising a device provider' do
         expected_values = 'device_provider { \'wibble\': \n\s+ensure => \'present\',\n\s+string => \'sample\',\n\#\s+string_ro => \'fixed\', # Read Only\n  string_param => \'default value\',\n}'
         fiddle_deprecate_msg = "DL is deprecated, please use Fiddle\n"
         win32_deprecate_msg = ".*Struct layout is already defined for class Windows::ServiceStructs::SERVICE_STATUS_PROCESS.*\n"
-        expect(stdout_str.strip).to match(/\A(#{fiddle_deprecate_msg}|#{win32_deprecate_msg})?#{expected_values}\Z/)
+        expect(stdout_str.strip).to match %r{\A(#{fiddle_deprecate_msg}|#{win32_deprecate_msg})?#{expected_values}\Z}
         expect(status).to eq 0
       end
 
@@ -63,40 +70,45 @@ RSpec.describe 'exercising a device provider' do
         expected_values = 'device_provider: |2\n\s+wibble:\n\s+ensure: :present\n\s+string: sample\n\s+string_ro: fixed\n\s+string_param: default value'
         fiddle_deprecate_msg = "DL is deprecated, please use Fiddle\n"
         win32_deprecate_msg = ".*Struct layout is already defined for class Windows::ServiceStructs::SERVICE_STATUS_PROCESS.*\n"
-        expect(stdout_str.strip).to match(/\A(#{fiddle_deprecate_msg}|#{win32_deprecate_msg})?#{expected_values}\Z/)
+        expect(stdout_str.strip).to match %r{\A(#{fiddle_deprecate_msg}|#{win32_deprecate_msg})?#{expected_values}\Z}
         expect(status).to eq 0
       end
 
       it 'deals with canonicalized resources correctly' do
         stdout_str, status = Open3.capture2e("puppet resource #{common_args} device_provider wibble ensure=present #{default_type_values}")
         stdmatch = 'Notice: /Device_provider\[wibble\]/string: string changed \'sample\' to \'changed\''
-        expect(stdout_str).to match(/#{stdmatch}/)
+        expect(stdout_str).to match %r{#{stdmatch}}
         expect(status).to be_success
       end
     end
   end
 
   describe 'using `puppet device`' do
-    let(:common_args) { "#{super()} --target the_node" }
+    let(:common_args) { super() + ' --target the_node' }
     let(:device_conf) { Tempfile.new('device.conf') }
     let(:device_conf_content) do
-      <<~DEVICE_CONF
-        [the_node]
-        type test_device
-        url  file://#{device_credentials.path}
-      DEVICE_CONF
+      <<DEVICE_CONF
+[the_node]
+type test_device
+url  file://#{device_credentials.path}
+DEVICE_CONF
     end
     let(:device_credentials) { Tempfile.new('credentials.txt') }
     let(:device_credentials_content) do
-      <<~DEVICE_CREDS
-        {
-          username: foo
-          secret: wibble
-        }
-      DEVICE_CREDS
+      <<DEVICE_CREDS
+{
+  username: foo
+  secret: wibble
+}
+DEVICE_CREDS
     end
 
-    before do
+    def is_device_apply_supported?
+      Gem::Version.new(Puppet::PUPPETVERSION) >= Gem::Version.new('5.3.6') && Gem::Version.new(Puppet::PUPPETVERSION) != Gem::Version.new('5.4.0')
+    end
+
+    before(:each) do
+      skip "No device --apply in puppet before v5.3.6 nor in v5.4.0 (v#{Puppet::PUPPETVERSION} is installed)" unless is_device_apply_supported?
       device_conf.write(device_conf_content)
       device_conf.close
 
@@ -104,7 +116,7 @@ RSpec.describe 'exercising a device provider' do
       device_credentials.close
     end
 
-    after do
+    after(:each) do
       device_conf.unlink
       device_credentials.unlink
     end
@@ -122,9 +134,9 @@ RSpec.describe 'exercising a device provider' do
         f.close
 
         stdout_str, _status = Open3.capture2e("puppet device #{common_args} --deviceconfig #{device_conf.path} --apply #{f.path}")
-        expect(stdout_str).to match(/Compiled catalog for the_node/)
-        expect(stdout_str).to match(/defined 'message' as 'foo'/)
-        expect(stdout_str).not_to match(/Error:/)
+        expect(stdout_str).to match %r{Compiled catalog for the_node}
+        expect(stdout_str).to match %r{defined 'message' as 'foo'}
+        expect(stdout_str).not_to match %r{Error:}
       end
     end
 
@@ -134,7 +146,7 @@ RSpec.describe 'exercising a device provider' do
         f.close
 
         stdout_str, status = Open3.capture2e("puppet device #{common_args} --deviceconfig #{device_conf.path} --apply #{f.path}")
-        expect(stdout_str).not_to match(/Error:/)
+        expect(stdout_str).not_to match %r{Error:}
         expect(status).to eq 0
       end
     end
@@ -143,13 +155,13 @@ RSpec.describe 'exercising a device provider' do
       it 'applies the catalog successfully' do
         Tempfile.create('fact_set') do |f|
           f.write 'device_provider{ "foo":' \
-                  'ensure => "present", boolean => true, integer => 15, float => 1.23, variant_pattern => "0x1234ABCD", ' \
-                  'url => "http://www.google.com", boolean_param => false, integer_param => 99, float_param => 3.21, ' \
-                  'ensure_param => "present", variant_pattern_param => "9A2222ED", url_param => "http://www.puppet.com" }'
+            'ensure => "present", boolean => true, integer => 15, float => 1.23, variant_pattern => "0x1234ABCD", '\
+            'url => "http://www.google.com", boolean_param => false, integer_param => 99, float_param => 3.21, '\
+            'ensure_param => "present", variant_pattern_param => "9A2222ED", url_param => "http://www.puppet.com" }'
           f.close
 
           stdout_str, _status = Open3.capture2e("puppet device #{common_args} --deviceconfig #{device_conf.path} --apply #{f.path}")
-          expect(stdout_str).not_to match(/Error:/)
+          expect(stdout_str).not_to match %r{Error:}
         end
       end
     end
